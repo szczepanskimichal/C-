a460d3f6f656a057a905a226948eb43123d5ad82
Michal Szczepanski <127307935+szczepanskimichal@users.noreply.github.com>
2026-01-23 12:07:48 +0100

    Add Ex4 dice game
    Roll three d6, print rolls and total;

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+if (total > 14)
+{
+    Console.WriteLine("You won!!!");
+}
+if (total < 14)
+{
+    Console.WriteLine("You lost!!!");
+}
+
+Console.WriteLine($"You rolled: {roll1}, {roll2}, {roll3} = {total}");
