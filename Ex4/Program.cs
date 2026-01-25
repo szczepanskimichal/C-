f7bc675078f935e22a674b72e111b1f9dcdb5fa5
Michal Szczepanski <127307935+szczepanskimichal@users.noreply.github.com>
2026-01-25 17:40:00 +0100

    Add dice game logic: rolls, bonuses and messages
    Rolls three dice, sums values, adds +6 for triples and +2 for doubles. Prints rolled values and a win message when total >= 15.

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -6,13 +6,26 @@ int roll3 = dice.Next(1, 7);
 
 int total = roll1 + roll2 + roll3;
 
-if (total > 14)
+if ((roll1 == roll2 || roll1 == roll3 || roll2 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total += 2;
+    }
+}
+
+if (total >= 15)
 {
     Console.WriteLine("You won!!!");
-}
-if (total < 14)
+} 
+else 
 {
     Console.WriteLine("You lost!!!");
 }
-
 Console.WriteLine($"You rolled: {roll1}, {roll2}, {roll3} = {total}");
