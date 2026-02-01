bbe70b78e0fc3f6104bc1c84c5a3e640e8e6870c
michalszczepanski <michalszczepanski07@gmail.com>
2026-02-01 14:10:23 +0100

    - Add role checks for Admin and Manager and output appropriate access messages. - Enforce level thresholds for Manager and provide special greeting for Super Admin. - Fix typos in variable names and output text, remove unused using and clean up commented examples. - Improve readability and formatting.

---
+++ ex10/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+
+string value1="a";
+string value2="A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());*/
+/*
+string pangram = "The quick brown fox jumps over the lazy dog";
+Console.WriteLine(pangram.Contains("fox")==false);
+Console.WriteLine(pangram.Contains("cat"));*/
+
+using System.Collections.Concurrent;
+/*
+Random coin = new Random();
+//int flip=coin.Next(0,2);
+//Console.WriteLine((flip==0)?"heads":"tails");
+Console.WriteLine((coin.Next(0, 2)==0) ? "heads" : "tails");
+*/
+string perrmision ="Admin | Manager";
+int level = 135;
+if (perrmision.Contains("Admin"))
+{
+    if (level==55)
+    {
+        Console.WriteLine("Welcomen SUPER Admin with full access");
+    }
+    else
+    {
+        Console.WriteLine("Welcome Admin");
+    }
+}
+else if (perrmision.Contains("Manager"))
+{
+    if(level>=20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
